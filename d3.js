5f8ec40c24864e9532dfa40f4db77343007cdc1a
Erin Black <erinblackdesign@gmail.com>
2018-03-29 18:39:02 -0500

    added in randomly generated rectangles in intervals

--- d3.js
+++ d3.js
@@ -26,5 +26,89 @@
       .attr('width', 50)
       .attr('height', 100)
       .attr('fill', 'red')
-      .attr('fill-opacity', .5);
+      .attr('fill-opacity', '.5');
+
+
+    var data = [
+      {
+        x:10,
+        y:10,
+        width:5,
+        height:40
+      },{
+        x:40,
+        y:10,
+        width:100,
+        height:40
+      }
+    ];
+
+    var svg3 = d3.select('.thirdRectangle')
+      .append('svg')
+      .attr('width', 200)
+      .attr('height', 200);
+
+    svg3.selectAll('rect').data(data).enter()
+      .append('rect')
+      .attr('x', function(d){ return d.x})
+      .attr('y', function(d){ return d.y})
+      .attr('fill', 'green')
+      .attr('fill-opacity', '.5')
+      .attr("width", function(d){ return d.width})
+      .attr("height", function(d){ return d.height});
+
+
+      var makeData = function(n){
+        var arr = [];
+
+        for (var i=0; i<n; i++){
+          arr.push({
+            x:Math.floor((Math.random() * 100) + 1),
+            y:Math.floor((Math.random() * 100) + 1),
+            width:Math.floor((Math.random() * 100) + 1),
+            height:Math.floor((Math.random() * 100) + 1)
+          })
+        };
+
+        return arr;
+      }
+
+      var rectangles = function(svg) {
+        var data = makeData(2);
+
+        var rect = svg.selectAll('rect').data(data);
+
+        // Enter
+        rect.enter().append('rect')
+          .attr('test', function(d,i) {
+            // Enter called 2 times only
+            console.log('enter placing initial rectangle: ', i)
+        });
+
+        // Update
+        rect.transition().duration(500).attr('x', function(d){ return d.x; })
+            .attr('y', function(d){ return d.y; })
+            .attr('width', function(d){ return d.width; })
+            .attr('height', function(d){ return d.height; })
+            .attr('test', function(d, i) {
+              // update every data change
+              console.log('updating x position to: ', d.x)
+            });
+        // Exit
+        rect.exit().attr('test', function(d) {
+          console.log('no data...')
+        }).remove();
+      };
+
+      var svg = d3.select("body")
+          .append("svg")
+          .attr("width", 200)
+          .attr("height", 200);
+
+      rectangles(svg);
+
+      setInterval(function(){
+        rectangles(svg);
+      },1000);
+
 })();
